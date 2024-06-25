eb629c69dcf31064c7e13bb4813b0730b180af61
223880 <rafael.segala45@gmail.com>
2024-06-25 12:54:01 -0300

    Fix

---
+++ src/use case/miniscript.js
@@ -0,0 +1,7 @@
+const { Miniscript } = require('miniscript')
+const { compile } = require('miniscript-compiler')
+
+const miniscript = new Miniscript()
+
+const if miniscript.compile('pk(0202020202020202020202020202020202020202020202020202020202020202)')
+
