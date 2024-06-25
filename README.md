e8e7b050798ca239c9ca073220dc09d4851ecc20
223880 <rafael.segala45@gmail.com>
2024-06-25 13:21:25 -0300

    Fix

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Nostr Smart Contract :coin:
 
 Mini demo/PoC for smart contracts using Nostr over Bitcoin timechain
+
+## About 
+
+This is a simple PoC for smart contracts using Nostr over Bitcoin timechain.
+
+## Libraries used
+
+- Bitcoinjs-lib
+- NDK
+
+## License
+
+MIT
--- main.js
+++ main.js
@@ -17,7 +17,7 @@ async function generateBitcoinWallet() {
   const keyPair = root.derivePath("m/49'/1'/0'/0/0");
   const { address } = bitcoin.payments.p2wpkh({
     pubkey: keyPair.publicKey,
-    network: bitcoin.networks.testnet,
+    network: bitcoin.networks.signet,
   });
 
   return { mnemonic, address };
@@ -55,9 +55,9 @@ function createSmartContract() {
         Buffer.from('02c0ded5a4c85d1d0c4b2a2a2b1ddf202d49c41e1e6471c379f027eaa7d8c46b6a', 'hex'),
         Buffer.from('02d0e1b9e7b1a5a8d6f14d3f5c8a86a5d2d1e3b5d4c7d1e9e1d4b2c1d0a9e3f7c2', 'hex')
       ],
-      network: bitcoin.networks.testnet,
+      network: bitcoin.networks.signet,
     }),
-    network: bitcoin.networks.testnet,
+    network: bitcoin.networks.signet,
   });
 
   return p2sh.address;
