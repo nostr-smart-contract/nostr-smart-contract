b6f2ed4d7c52acf7b0a4642a84c7fc6459f6596a
223880 <rafael.segala45@gmail.com>
2024-08-12 06:48:26 -0300

    Fix

---
+++ src/use case/token.js
@@ -0,0 +1,7 @@
+
+import { NostrSmartContract } from '@nostr-smart-contract/nostr-smart-contract';
+import { Glaphy } from 'glaphy';
+export class Token extends NostrSmartContract {
+  constructor(glaphy: Glaphy) {
+    super(glaphy);
+  }
