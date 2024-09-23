fd27b2d7858514cf8382ee648914fa7aa1e47692
223880 <rafael.segala45@gmail.com>
2024-09-23 06:52:56 -0300

    Add P2TR contract

--- src/contract/smartContract.js
+++ src/contract/smartContract.js
@@ -1,4 +1,5 @@
 const bitcoin = require('bitcoinjs-lib');
+const { p2tr } = require('bitcoinjs-lib/src/payments');
 const ecc = require('tiny-secp256k1');
 
 // Example of a simple smart contract using Bitcoin script
@@ -21,6 +22,27 @@ function verifySignature(message, signature, publicKey) {
   return ecc.verify(message, publicKey, signature);
 }
 
+module.exports = {
+  createSmartContract,
+  verifySignature,
+};
+
+// Simple contract using P2TR
+
+function createSmartContract(){
+  const p2tr = bitcoin.payments.p2tr({
+    script: bitcoin.script.compile([
+      bitcoin.opcodes.OP_1,
+      bitcoin.opcodes.OP_EQUAL,
+    ]),
+  });
+}
+return p2tr;
+
+// Verify signature using elliptic curve
+function verifySignature(message, signature, publicKey) {
+  return ecc.verify(message, publicKey, signature);
+}
 module.exports = {
   createSmartContract,
   verifySignature,
