bb59452d1ac4b78d119d84edc8708c6f0a67d1ae
22388o <22388o@users.noreply.github.com>
2025-02-12 06:31:57 -0300

    Initial LN support

--- src/use case/ln.js
+++ src/use case/ln.js
@@ -1,4 +1,16 @@
 
 import { NostrSmartContract } from '@nostr-smart-contract/nostr-smart-contract';
 
-import 
+import { LNPaymentRequest } from '@nostr-dev-kit/ndk';
+
+import { lightningLabsSwapService } from 'goldengate';
+
+import { getLockedUtxos, getLockedUtxos } from 'ln-service';
+
+const LNPaymentRequest = new LNPaymentRequest
+
+const lightningLabsSwapService = new lightningLabsSwapService
+
+const getLockedUtxos = new getLockedUtxos
+
+const NostrSmartContract = new NostrSmartContract
