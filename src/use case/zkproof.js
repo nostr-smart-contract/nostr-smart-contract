c50dbafba54eeb1ae701b2f17746f7c40b82df06
22388o <22388o@users.noreply.github.com>
2025-04-22 05:32:08 -0300

    Update deps and initial zk proof

--- src/use case/zkproof.js
+++ src/use case/zkproof.js
@@ -1,6 +1,13 @@
 const zkproof = require('./zkproof.js');
 
 
+const proof = {
+    InputDeviceInfo: String,
+    OutputDeviceInfo: String,
+    Event: String,
+    Address: String
+}
+
 module.exports = {
     zkproof
 }
